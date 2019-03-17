e0375796e31b4341b705de90efa0466cf21f6fd7
Sean Klechak <sklechak@students.mchenry.edu>
2019-03-17 13:52:41 -0500

    Initial commit

---
+++ Fonts/FavoritesList.swift
@@ -0,0 +1,42 @@
+//
+//  FavoritesList.swift
+//  Fonts
+//
+//  Created by student on 3/17/19.
+//  Copyright © 2019 Sean Klechak. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FavoritesList {
+    static let sharedFavoritesList = FavoritesList()
+    private(set) var favorites:[String]
+    
+    init() {
+        let defaults = UserDefaults.standard
+        let storedFavorites = defaults.object(forKey: "favorites") as? String
+        favorites = storedFavorites != nil ? [storedFavorites!] : []
+    
+        }
+    
+    func addFavorite(fontName: String) {
+        if !favorites.contains(fontName) {
+            favorites.append(fontName)
+                saveFavorites()
+            }
+        }
+        
+        func removeFavorite(fontName: String){
+            if let index = favorites.index(of: fontName) {
+                favorites.remove(at: index)
+                saveFavorites()
+            }
+        }
+    
+        private func saveFavorites() {
+            let defaults = UserDefaults.standard
+            defaults.set(favorites, forKey: "favorites")
+            defaults.synchronize()
+        }
+    }
